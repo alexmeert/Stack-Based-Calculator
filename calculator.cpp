82974cb3eb2383d3aaf87e2a720df16ee510c90a
Nicholas Alexander Meert <115733315+alexmeert@users.noreply.github.com>
2024-02-20 11:02:14 -0500

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -6,29 +6,50 @@ using namespace std;
 
 class Stack
 {
+    private:
+
+    static const int MAX_SIZE = 100;
+    int data[MAX_SIZE];
+    int top;
+
     public:
 
     int top = -1;
 
     void push(int val) // adds a new value to the top of the stack
     {
-        if(top >= )
+        if(top >= MAX_SIZE -1)
+        {
+            cout << "Stack Overflow!" << endl;
+            return;
+        }
+        data[++top] = val;
     }
 
     void pop() // removes the value at the top of the stack
     {
-
+        if(top < 0)
+        {
+            cout << "Stack Underflow!" << endl;
+            return;
+        }
+        top--;
     }
 
-    void peek() // returns the value at the top of the stack w/o removing it
+    int peek() // returns the value at the top of the stack w/o removing it
     {
-
+        if(top < 0)
+        {
+            cout << "Stack is empty!" << endl;
+            return -1;
+        }
+        return data[top];
     }
 
 
     string to_postfix(string s) // transfroms an infix expression to a postfix expression
     {
-
+        
     }
 
     string print_postfix(string s) // prints the postfix expression
